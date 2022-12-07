53348991dd13156eae0fcc551d38d014615df845
George Boyle <george@spotify.com>
2022-12-07 11:58:34 -0500

    Day 4

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod parser;
 mod q01;
 mod q02;
 mod q03;
+mod q04;
 
 fn print_result(number: u8, part1: &impl ToString, part2: &impl ToString) {
     println!("Question {:}", number);
@@ -18,5 +19,6 @@ fn main() -> std::io::Result<()> {
     print_result(1, &q01::part_1()?, &q01::part_2()?);
     print_result(2, &q02::part_1()?, &q02::part_2()?);
     print_result(3, &q03::part_1()?, &q03::part_2()?);
+    print_result(4, &q04::part_1()?, &q04::part_2()?);
     Ok(())
 }
