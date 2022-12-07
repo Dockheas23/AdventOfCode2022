72d2c25002985453a0abff7eb4bb39196ac64183
George Boyle <george@spotify.com>
2022-12-07 11:54:47 -0500

    Add input parser and prepare main file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod parser;
+
+fn print_result(number: u8, part1: &impl ToString, part2: &impl ToString) {
+    println!("Question {:}", number);
+    println!(
+        "Part 1: {:}; Part 2: {:}",
+        part1.to_string(),
+        part2.to_string()
+    );
+    println!("------------");
+}
+
 fn main() -> std::io::Result<()> {
+    println!("------------");
     Ok(())
 }
